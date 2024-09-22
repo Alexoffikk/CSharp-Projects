9b8caf43963ebf051b780e03fa84c3b651b7a8cf
Alexoffikk <allexxoffikk@gmail.com>
2024-09-22 15:55:00 +0500

    Иконка приложения

--- CalculatorOnWPF/obj/Debug/net8.0-windows/CalculatorOnWPF.AssemblyInfo.cs
+++ CalculatorOnWPF/obj/Debug/net8.0-windows/CalculatorOnWPF.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CalculatorOnWPF")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6a73fb175cc9055c0532d822dae3c5fb5ccb235")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f291e5813b590b43ef3896ecd308be995c354a2")]
 [assembly: System.Reflection.AssemblyProductAttribute("CalculatorOnWPF")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CalculatorOnWPF")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
